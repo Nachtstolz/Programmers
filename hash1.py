8b787a8ec5461a176dc2e56cfe9715a240ae9ec5
Nachtstolz <sayong4424@gmail.com>
2024-10-02 02:19:15 +0900

    241002 :: the problem of hash is solved by myself

---
+++ hash1.py
@@ -0,0 +1,13 @@
+# 문제 링크 | https://school.programmers.co.kr/learn/courses/30/lessons/1845
+
+def solution(nums):
+    
+    n = len(nums)
+    answer = n//2
+    
+    tmp_set = set(nums)
+    # print(tmp_set)
+    if len(tmp_set) < answer : 
+        answer = len(tmp_set)
+    
+    return answer
