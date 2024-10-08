aa9cbeea22b9fe328c059fdf2dc909a1f182e4e7
Nachtstolz <sayong4424@gmail.com>
2024-10-08 13:17:46 +0900

    241008 :: the problem of hash is solved - using sort and a single loop

---
+++ hash3.py
@@ -0,0 +1,31 @@
+# 전화번호 목록 # 성공
+# 문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/42577#
+
+def solution(phone_book):
+    answer = True
+    
+    # 직접 비교 : 효율성 쓰레기
+
+    '''
+    # 길이 순으로 정렬
+    phone_book.sort(key = lambda phone:len(phone))    
+
+    for i in range(len(phone_book)) :
+        length = len(phone_book[i])
+        for j in range(i+1, len(phone_book)) :
+            if phone_book[i] == phone_book[j][:length] :
+                answer = False
+                return answer
+    '''
+    
+    ''' 정렬 -> 반복문 '''
+    phone_book.sort()
+    
+    # 단방향 비교만 해도 괜찮다
+    for i in range(len(phone_book)-1) :
+        length = len(phone_book[i])
+        if phone_book[i] == phone_book[i+1][:length] :
+            answer = False
+            return answer
+
+    return answer
